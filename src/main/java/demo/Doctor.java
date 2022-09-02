39ba28aca9f943f12115fc186a1531af530bf4fd
bemiyaz <benito.miya172@gmail.com>
2022-09-02 12:50:40 -0300

    beans scope definition

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -1,7 +1,21 @@
 package demo;
 
-public class Doctor {
-    public void assist(){
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(scopeName = "singleton")
+public class Doctor implements Staff {
+    private String qualification;
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public void assist() {
         System.out.println("Doctor is assisting!");
     }
 }
--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,8 +1,18 @@
 package demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor doctor = new Doctor();
-        doctor.assist();
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+
+        Doctor doctor1 = context.getBean(Doctor.class);
+        doctor1.setQualification("Qualification X");
+        System.out.printf("Doctor1: Qualification: %s\n", doctor1.getQualification());
+
+        Doctor doctor2 = context.getBean(Doctor.class);
+        System.out.printf("Doctor2: Qualification: %s", doctor2.getQualification());
     }
 }
